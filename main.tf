b2f76387dfe08d8f604d40264e51b733d6804c9a
Elywen <simplon.luna@gmail.com>
2023-10-05 11:58:28 +0200

    initial 0.0.0.0

--- main.tf
+++ main.tf
@@ -12,13 +12,14 @@ module "azure_infra" {
   address_space           = ["10.0.0.0/16"]
   subnet_name             = "${var.prefix}-Subnet"
   subnet_address_prefixes = ["10.0.1.0/24"]
-  aks_name                = "${var.prefix}-AKSCluster"
-  node_pool_name          = "default"
-  node_count              = 2
+  ## aks_name                = "${var.prefix}-AKSCluster"
+  ## node_pool_name          = "default"
+  ## node_count              = 2
   vm_size                 = "Standard_DS2_v2"
   use_nat_gateway         = true
   nat_gateway_name        = "${var.prefix}-NATGateway"
   nginx_ingress_ip_name   = "${var.prefix}-IngressIp"
   nginx_ingress_ip_sku    = "Standard"
   route_table_name        = "customRouteTableName"
+  vm_name                 = "ely13-vm"
 }
